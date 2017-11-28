9996918e08c2eb8e077dcee6224d0527a7ed33cf
abhijithmandya17 <abhijithmanday17@gmail.com>
2017-11-28 01:35:24 -0500

    SVM-R Update

--- Code/R/Random Forest.R
+++ Code/R/Random Forest.R
@@ -1,3 +1,4 @@
+##################### Libraries #####################
 library(randomForest)
 library(ranger)
 library(plyr)
@@ -9,19 +10,14 @@ library(pROC)
 library(parallel)
 library(doParallel)
 
+
+##################### Data Exploration #####################
+
 all_data <- read.csv('mal_and_benign_traces.csv', header=T) #reading in the data
-
-#Create test set for cross validation using gini scores
-set.seed(134)
-
-train_index <- sample(1:nrow(all_data), 3000, replace= FALSE)
-
-caret_data <- all_data[train_index,-c(2:6)]
-caret_test <- all_data[-train_index,-c(2:6)]
 
 #First run to check variable importance
 set.seed(12)
-rf1 <- randomForest(caret_data[,-1], caret_data[,1], mtry = 2, ntree = 300)
+rf1 <- randomForest(all_data[,-1], all_data[,1], mtry = 2, ntree = 300)
 var_imp <- varImpPlot(rf1, sort = TRUE, main = "Variable Importance")
   
 
@@ -43,6 +39,55 @@ boxplot(all_data[all_data[,"mean_duration"],"mean_duration"]~all_data[all_data[,
         col=(c("darkgreen","red")),
         main="Mean Duration Distribution", xlab = "Non Malicious/Malacious", ylab = "Mean Duration")
 
+
+
+
+
+##################### Splitting Data #####################
+set.seed(134)
+
+train_index <- sample(1:nrow(all_data), 3000, replace= FALSE)
+
+caret_data <- all_data[train_index,-c(2:6)]
+caret_test <- all_data[-train_index,-c(2:6)]
+
+
+
+##################### Caret Implementation of Radial SVM ##################### 
+
+grid_radial <- expand.grid(sigma = c(0,0.01, 0.1,1), C = c(0.01, 0.1,1, 2,5))
+
+svm_Radial <- train(as.factor(Malicious) ~., data = caret_data, method = "svmRadial",
+                    trControl=control,
+                    preProcess = c("center"),
+                    tuneGrid = grid_radial,
+                    tuneLength = 10)
+
+print(svm_Radial)
+
+#test
+test_pred_svmr <- predict(svm_Radial, newdata = caret_test[,-1])
+confusionMatrix(test_pred_svmr, as.factor(caret_test[,1]))
+
+##################### SVM-R Results #####################
+# Confusion Matrix and Statistics
+# 
+#               Reference
+# Prediction    0    1
+#           0 1058  207
+#           1    0   10
+# 
+# Accuracy : 0.8376          
+# 95% CI : (0.8162, 0.8575)
+# Sensitivity : 1.00000         
+# Specificity : 0.04608         
+# Pos Pred Value : 0.83636 (Correct Benign)         
+# Neg Pred Value : 1.00000  (Correct Malicious)      
+
+#Balanced Accuracy : 0.52304 
+
+
+##################### Caret Implementation of Random Forest #####################
 
 #Create custom RF function for grid search in Caret 
 customRF <- list(type = "Classification", library = "randomForest", loop = NULL)
@@ -75,8 +120,30 @@ plot(rf_gridsearch)
 
 #Create randomforest on optimum mtry and ntrees
 crf <- randomForest(caret_data[,-1], as.factor(caret_data[,1]), mtry = 6, ntree = 200)
+
 #test
 test_pred_crf <- predict(crf, newdata = caret_test[,-1])
 confusionMatrix(test_pred_crf, as.factor(caret_test[,1]))
 
-roc(caret_test[,1]~as.numeric(test_pred_crf), percent = TRUE, smooth = F, plot = TRUE)
+##################### Random Forest Results #####################
+# Confusion Matrix and Statistics
+# 
+#              Reference
+# Prediction    0    1
+#            0 1056    6
+#            1    2  211
+# 
+# Accuracy : 0.9937          
+# 95% CI : (0.9877, 0.9973)
+# No Information Rate : 0.8298          
+# P-Value [Acc > NIR] : <2e-16          
+
+# Sensitivity : 0.9981          
+# Specificity : 0.9724          
+# Pos Pred Value : 0.9944 (Correct Benign)         
+# Neg Pred Value : 0.9906 (Correct Malicious)          
+
+# Balanced Accuracy : 0.9852          
+
+
+
